f165f211afc540104ea3786b33b81406b73fc1f2
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-11 23:29:07 +0600

    https://lightoj.com/problem/trailing-zeroes-iii
    Lightoj Trailling Zeroes-iii problem solution with link

--- code.cpp
+++ code.cpp
@@ -5,45 +5,65 @@ using namespace std;
 #define pb push_back
 #define ps push
 #define pp pop
-map<ll,vector<ll> >m1;
-map<ll,ll>m;
-
-
 
 vector<ll>v;
+vector<ll>:: iterator it;
+
+ll zero(ll n)
+{
+    ll i,c=0,pw=5;
+    while(n>=pw)
+    {
+       c+=n/pw;
+       pw=pw*5;
+    }
+    return  c;
+}
+ll bsearch(ll q)
+{
+    ll l=0,r=1e9,mid;
+    while(l<=r)
+    {
+        mid=(l+r)/2;
+        if(zero(mid)>q)
+            r=mid-1;
+        else if(zero(mid)<q)
+            l=mid+1;
+        else
+            return mid;
+    }
+    return -1;
+}
+
+
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll t,i,n,ans,j,a,b,c,p=1,sum;
+    ll t,n,c=1,a,i;
     cin>>t;
     while(t--)
     {
         cin>>n;
-        v.clear();
-        ans=0;
-        for(i=0; i<n; i++)
+        if(bsearch(n)==-1)
+            cout<<"Case "<<c<<": impossible"<<endl;
+        else
         {
-            cin>>a;
-            v.pb(a);
-        }
-        sort(v.begin(),v.end());
-        for(i=0; i<n-2; i++)
-        {
-            for(j=i+1; j<n-1; j++)
+            a=bsearch(n);
+            for(i=a; i>=1; i--)
             {
-                a=v[i];
-                b=v[j];
-                sum=a+b;
-                c=lower_bound(v.begin()+j+1,v.end(),sum)-v.begin();
-                ans+=c-j-1;
+                if(zero(i)!=n)
+                    break;
             }
+            cout<<"Case "<<c<<": "<<i+1<<endl;
+
         }
-        cout<<"Case "<<p<<": "<<ans<<endl;
-        p++;
+        c++;
 
     }
-return 0;
+
+
+    return 0;
 }
